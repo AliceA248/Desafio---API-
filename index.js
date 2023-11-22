c041e75b46c6a1133b16ff59c1049d69fd5aa2ef
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-11-22 15:33:01 -0300

    configuração inicial - router

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const users = [
-    { id: 1, username: "user1", password: "password1" },
-    { id: 2, username: "user2", password: "password2" }
-];
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (user) {
-        res.json({ message: 'Login bem-sucedido!' });
-    } else {
-        res.status(401).json({ message: "Login falhou" });
-    }
+app.get('/', (req, res) => {
+  res.send('Tudo certo!');
 });
 
-app.post('/signup', (req, res) => {
-    const { username, password } = req.body;
-
-    if (users.some(u => u.username === username)) {
-        res.status(400).json({ message: "Usuário já existe" });
-    } else {
-        const newUser = { id: users.length + 1, username, password };
-        users.push(newUser);
-        res.json({ message: "Cadastro bem-sucedido!", user: newUser });
-    }
-});
-
-app.listen(port, () => {
-    console.log("Servidor rodando na porta ${port}");
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
 });
---
+++ routers/authRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+});
+
+router.post('/signin', (req, res) => {
+});
+
+router.get('/user', (req, res) => {
+});
+
+module.exports = router;
